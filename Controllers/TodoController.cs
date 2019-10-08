e0e64ba25bcb49c7c59ee76f6baedb20e53e65d2
jakegalligan <galligan1996@gmail.com>
2019-10-08 10:55:43 -0400

    Create a delete method

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -61,5 +61,20 @@ namespace TodoApi.Controllers
 
             return NoContent();
         }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+            
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todoItem);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
     }
 }
